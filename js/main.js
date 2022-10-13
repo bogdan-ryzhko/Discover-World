b22da55cd1411215b910e85482b1db155d2be387
Bogdan Ryzhko <m3meizumini@gmail.com>
2022-10-13 22:18:29 +0300

    add the bottom of the page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ window.addEventListener('DOMContentLoaded', function () {
 	const likeIcon = this.document.querySelectorAll('.btn-like-icon');
 	const bellBtn = this.document.querySelector('.bell__btn');
 	const bellBtnIcon = this.document.querySelector('.bell__btn-icon');
+	const toursBtn = this.document.querySelectorAll('.tours__btn');
+	const toursItem = this.document.querySelectorAll('.tours__item');
 
 	bellBtn.addEventListener('click', function () {
 		bellBtn.classList.toggle('active');
@@ -12,8 +14,16 @@ window.addEventListener('DOMContentLoaded', function () {
 	for (let i = 0; i < likeBtn.length; i++) {
 		likeBtn[i].addEventListener('click', function () {
 			for (let g = 0; g < likeIcon.length; g++) {
-				likeIcon[i].classList.toggle('active')
+				likeIcon[i].classList.toggle('active');
 			}
 		});
 	};
-})
+
+	for (let i = 1; i < toursBtn.length; i++) {
+		toursBtn[i].addEventListener('click', function () {
+			for (let g = 0; g < toursItem.length; g++) {
+				toursItem[i].classList.toggle('tours__item-current');
+			}
+		});
+	};
+});
